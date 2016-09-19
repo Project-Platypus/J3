8514552545d65aa05ad6f7891e4882efed448fe6
David Hadka <dmh309@arl.psu.edu>
2016-09-19 13:52:28 -0400

    Make magic typing class more flexible...still not great, but more configurable

--- src/j3/dataframe/MagicTyping.java
+++ src/j3/dataframe/MagicTyping.java
@@ -1,15 +1,22 @@
 package j3.dataframe;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class MagicTyping {
 	
-	public enum Type {
+	private List<Pair<Predicate<? super Object>, Class<? extends Attribute<?>>>> conversions;
+	
+	public MagicTyping() {
+		super();
 		
-		DOUBLE,
-		
-		STRING
-		
+		conversions = new ArrayList<>();
+		conversions.add(new ImmutablePair<>(isInteger, IntegerAttribute.class));
+		conversions.add(new ImmutablePair<>(isDouble, DoubleAttribute.class));
 	}
 
 	public void convert(DataFrame frame) {
@@ -29,13 +36,17 @@ public class MagicTyping {
 	}
 	
 	private Attribute<?> determineType(DataFrame frame, Attribute<?> attribute) {
-		boolean isDouble = frame.getInstances().stream().map(i -> (Object)i.get(attribute)).allMatch(this.isDouble);
-		
-		if (isDouble) {
-			return new DoubleAttribute(attribute.getName());
-		} else {
-			return attribute;
+		for (Pair<Predicate<? super Object>, Class<? extends Attribute<?>>> conversion : conversions) {
+			if (frame.getInstances().stream().map(i -> (Object)i.get(attribute)).allMatch(conversion.getKey())) {
+				try {
+					return conversion.getValue().getDeclaredConstructor(String.class).newInstance(attribute.getName());
+				} catch (Exception e) {
+					throw new RuntimeException("Unable to call constructor for " + conversion.getValue());
+				}
+			}
 		}
+		
+		return attribute;
 	}
 	
 	private static Predicate<? super Object> isDouble = value -> {
@@ -45,7 +56,22 @@ public class MagicTyping {
 			try {
 				Double.parseDouble(value.toString());
 				return true;
-			} catch (NumberFormatException e) {
+			} catch (NumberFormatException ex) {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	};
+	
+	private static Predicate<? super Object> isInteger = value -> {
+		if ((value instanceof Integer) || (value instanceof Short) || (value instanceof Byte)) {
+			return true;
+		} else if (value instanceof String) {
+			try {
+				Integer.parseInt(value.toString());
+				return true;
+			} catch (NumberFormatException ex) {
 				return false;
 			}
 		} else {
