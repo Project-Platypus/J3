55c3dd59133cf2c3e9d50ae404ba3c63e82389aa
Dave Hadka <dhadka@github.com>
2023-12-24 20:17:03 -0700

    Update to use JLink / JPackage

--- package/j3.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-if not defined JAVAFX_HOME echo JAVAFX_HOME environment variable must be set!
-
-java --module-path %JAVAFX_HOME%\lib --add-modules javafx.controls --class-path "lib/*" j3.GUI
